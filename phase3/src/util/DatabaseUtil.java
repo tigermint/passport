28cbf0f3e36a211bee56f525f910b9874ae127e3
sami355-24 <ddung355@gmail.com>
2022-11-19 14:32:13 +0900

    feat: ReleaseDataBase, you can Release DB by this method

--- phase3/src/util/DatabaseUtil.java
+++ phase3/src/util/DatabaseUtil.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.sql.*;
+import java.util.logging.Logger;
 
 public class DatabaseUtil {
     public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -16,5 +17,25 @@ public class DatabaseUtil {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static Boolean ReleaseDataBase(Connection conn, Statement stmt) {
+        Logger Log = Logger.getGlobal();
+
+        try {
+            // Close the Statement object.
+            stmt.close();
+            Log.info("close stmt success");
+            // Close the Connection object.
+            conn.close();
+            Log.info("close conn success");
+            return true;
+
+        } catch (SQLException e) {
+            Log.warning("close conn or stmt failed");
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
     }
 }
