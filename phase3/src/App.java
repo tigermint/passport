7d9a55f7216efbbfed8e74f8ef8f69c2d0542242
sami355-24 <ddung355@gmail.com>
2022-11-19 19:27:52 +0900

    feat: Correct the code to prevent errors with the if statement

--- phase3/src/App.java
+++ phase3/src/App.java
@@ -1,14 +1,22 @@
 import java.sql.Connection;
+import java.util.logging.Logger;
 
 import static util.DatabaseUtil.getConnection;
 
 public class App {
     public static void main(String[] args) {
 
+        Logger Log = Logger.getGlobal();
+
         // page 호출 -> main, main에서 컴포넌트화 된 구성 요소들 호출
         System.out.println("Hello world!");
         // 연결 테스트
         Connection conn = getConnection();
-        System.out.println("conn = " + conn);
+        if (conn != null) {
+            Log.info("Connection success");
+            return;
+        }
+        Log.warning("Connection failed");
+        return;
     }
 }
