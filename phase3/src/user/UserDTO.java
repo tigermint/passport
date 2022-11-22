dabe1d9f5745abe7185d2d4551e83a5ed596f2ac
LEESEUNGYEOL <lsy12210@gmail.com>
2022-11-23 00:06:57 +0900

    feat: UserDTO

--- phase3/src/user/UserDTO.java
+++ phase3/src/user/UserDTO.java
@@ -2,11 +2,34 @@ package user;
 
 public class UserDTO {
     private String id;
-    private String pw;
+    private String password;
+    private String name;
+    private String schoolEmail;
+    private String bankName;
+    private String bankAccount;
+    private boolean isActive;
 
-    public UserDTO(String id, String pw) {
+    public UserDTO() {
+    }
+
+    public UserDTO(String id, String password, String name, String schoolEmail, String bankName, String bankAccount) {
         this.id = id;
-        this.pw = pw;
+        this.password = password;
+        this.name = name;
+        this.schoolEmail = schoolEmail;
+        this.bankName = bankName;
+        this.bankAccount = bankAccount;
+        this.isActive = true;
+    }
+
+    public UserDTO(String id, String password, String name, String schoolEmail, String bankName, String bankAccount, boolean isActive) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.schoolEmail = schoolEmail;
+        this.bankName = bankName;
+        this.bankAccount = bankAccount;
+        this.isActive = isActive;
     }
 
     public String getId() {
@@ -17,11 +40,64 @@ public class UserDTO {
         this.id = id;
     }
 
-    public String getPw() {
-        return pw;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPw(String pw) {
-        this.pw = pw;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchoolEmail() {
+        return schoolEmail;
+    }
+
+    public void setSchoolEmail(String schoolEmail) {
+        this.schoolEmail = schoolEmail;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", schoolEmail='" + schoolEmail + '\'' +
+                ", bankName='" + bankName + '\'' +
+                ", bankAccount='" + bankAccount + '\'' +
+                ", isActive=" + isActive +
+                '}';
     }
 }
