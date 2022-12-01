c6c838b3ba6c423ff62ad2910635602c77c4dca8
Henry-Hong <hhr8540@naver.com>
2022-12-01 23:04:01 +0900

    feat: Login page added

--- phase4/client/src/index.jsx
+++ phase4/client/src/index.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "S-CoreDream-3Light",
+  },
+  palette: {
+    primary: {
+      main: "#4ABBFA",
+    },
+    secondary: {
+      main: "#edf2ff",
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'S-CoreDream-3Light';
+          src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+          font-weight: normal;
+          font-style: normal;
+        }
+      `,
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
