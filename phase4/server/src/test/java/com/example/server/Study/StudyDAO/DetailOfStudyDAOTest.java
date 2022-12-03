ae2e38d08f285ed3eb891d30ac03880309d2de93
sami355-24 <ddung355@gmail.com>
2022-12-03 20:35:11 +0900

    feat: DetailOfStudyDAO2 추가, 테스트 파일 추가

--- phase4/server/src/test/java/com/example/server/Study/StudyDAO/DetailOfStudyDAOTest.java
+++ phase4/server/src/test/java/com/example/server/Study/StudyDAO/DetailOfStudyDAOTest.java
@@ -1,12 +1,15 @@
 package com.example.server.Study.StudyDAO;
 
+import com.example.server.Study.StudyDTO.StudyDetailDTO;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.util.List;
 
 public class DetailOfStudyDAOTest {
 
     @Test
-    public void getStudyDetailDAOTest() {
+    public void getStudyDetailDAO1Test() {
 
         //given
         DetailOfStudyDAO1 DetailOfStudyDAO1 = new DetailOfStudyDAO1();
@@ -16,6 +19,20 @@ public class DetailOfStudyDAOTest {
 
         //then
         Assertions.assertThat(detailOfStudyDAO).isEqualTo("testUser3");
+
+    }
+
+    @Test
+    public void getStudyDetailDAO2Test() {
+
+        //given
+        DetailOfStudyDAO2 DetailOfStudyDAO2 = new DetailOfStudyDAO2();
+
+        //when
+        List<StudyDetailDTO> detailOfStudyDAO = DetailOfStudyDAO2.getDetailOfStudyDAO("1");
+
+        //then
+        Assertions.assertThat(detailOfStudyDAO.get(1).getAttendants()).isEqualTo(4);
 
     }
 }
