604376a705c17965cb06c2f45bea1484d395161d
LEESEUNGYEOL <lsy12210@gmail.com>
2022-12-08 02:37:47 +0900

    fix: memberController CrossOrigin error

--- phase4/server/src/main/java/com/example/server/controller/MemberController.java
+++ phase4/server/src/main/java/com/example/server/controller/MemberController.java
@@ -2,12 +2,12 @@ package com.example.server.controller;
 
 import com.example.server.domain.Member;
 import com.example.server.service.MemberService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
+@CrossOrigin("http://localhost:3000")
 public class MemberController {
 
     private final MemberService memberService;
